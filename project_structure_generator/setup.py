066613b96167f6ba5aa86836a739cd633b41751f
Vaishali Khandwala <Vaishali@softvan.in>
2024-06-27 16:06:30 +0530

    files added

--- project_structure_generator/setup.py
+++ project_structure_generator/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="project_structure_generator",
+    name="custom_project_structure_generator",
     version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
